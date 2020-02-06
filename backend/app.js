79dca9c57f180ce7aa49c2f98521e70a08b6f911
atmanand, sah <atmanand17@gmail.com>
2020-02-06 15:35:52 +0530

    connected the users database

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
+var {addAdmin} = require('./routes/create_users')
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
@@ -20,16 +21,28 @@ app.use((req,res,next)=>{
 
 
 
-mongoose.connect('mongodb://localhost/einn',{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>console.log("database connected"));
+mongoose.connect('mongodb://localhost/einn',{useNewUrlParser:true,useUnifiedTopology:true},function(err,db){
+    if(err){
+        console.log('unable to connect',err);
+    }
+    else{
+        console.log("database connnected");
+    }
+})
+//.then(()=>console.log("database connected"));
 
 
 app.get("/", (req, res)=>{
     res.send ("express");
 })
-app.post('/create_admin',function(req,res){
+app.post('/create_users',function(req,res){
     console.log('admin creation requested');
     console.log(req.body)
+
+    addAdmin(req.body)
+    .then((data)=>console.log(data))
+    .catch((err)=>console.log(err))
+
     var response_data = {
         ...req.body,
         created:"true"

4e810c0acf834182200ae9371a0a28fb0ee5d0cb
shubham0520 <59551953+shubham0520@users.noreply.github.com>
2020-04-01 14:17:35 +0530

    model of the admin login

---
+++ androidServer/model/AdminLogin.kt
@@ -0,0 +1,13 @@
+class AdminLogin (override val email : string , override val password : string) : IAdmin {
+
+    override fun isDatavalid() : int {
+        if(TextUtils.isEmpty(Email))
+            return 0 // 0 error code is empty
+        else if(!patterns.EMAIL_ADDRESS.matcher(email))
+            return 1 //1 error code is wrong pattern. means email pattern missmatch
+        else if (password.length<=6)
+            return 2 // 2 error code shows password must be greater then 6
+        else
+            return -1 // error code -1 is success code
+    }
+}

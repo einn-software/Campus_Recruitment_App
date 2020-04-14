148470c35e06443ca181450ada9250e1decac35a
shikhagupta2611 <shikha.gupta@einn.in>
2020-04-14 15:20:22 +0530

    new files

--- backend-test/Riya-test/server.js
+++ backend-test/Riya-test/server.js
@@ -1,5 +1,5 @@
 const app = require('./index');
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3700;
 const listener = app.listen(port, () => {
-    console.log("server up and running " + listener.address().port);
+    console.log("server up and running at port " + listener.address().port);
 });

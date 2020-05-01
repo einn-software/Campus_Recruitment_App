abc63fe1c5375fea7661a2d4e6ad20ef2279f1ba
shikhagupta2611 <60637506+shikhagupta2611@users.noreply.github.com>
2020-05-01 12:42:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,21 +17,14 @@ import Admin from "./components/Dashboard/admin";
 import TpoRegister from './components/register/tporegister';
 //import StudentRegister from './components/register/studentregister';
 
->>>>>>> 702626d9990f4554f2dfb0a5ce55050285833765
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
         <TpoRegister />
-=======
-<<<<<<< HEAD
         <Admin />
-=======
         <StudentLogin />
->>>>>>> 702626d9990f4554f2dfb0a5ce55050285833765
->>>>>>> d56346c002361f1dc60ec0f7192a745e2664d7d9
       </div>
     );
   }

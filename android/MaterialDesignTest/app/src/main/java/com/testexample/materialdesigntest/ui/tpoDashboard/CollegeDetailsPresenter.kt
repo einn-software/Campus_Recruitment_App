792f1df15ab2926b4fa318b1ab6bf41a82321e8e
shresthdeep gupta <shresthdeep.gupta@einn.in>
2020-06-26 04:59:52 +0530

    "Bug Fixing UI"

---
+++ android/MaterialDesignTest/app/src/main/java/com/testexample/materialdesigntest/ui/tpoDashboard/CollegeDetailsPresenter.kt
@@ -0,0 +1,71 @@
+package com.testexample.materialdesigntest.ui.tpoDashboard
+
+import android.annotation.SuppressLint
+import android.util.Log
+import android.widget.Toast
+import com.testexample.materialdesigntest.data.interactor.implementation.CollegeDetails
+import com.testexample.materialdesigntest.data.interactor.interfaces.ICollegeDetails
+import com.testexample.materialdesigntest.data.network.model.UpdateCollegeDetails
+import com.testexample.materialdesigntest.data.network.retrofit.handelNetworkError
+import com.testexample.materialdesigntest.data.session.SessionManager
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class CollegeDetailsPresenter(private var view: TPODashboardContract.CollegeDetailsView?) : TPODashboardContract.CollegeDetailsPresenter {
+
+    val TAG = "CollegeDetailsPresenter"
+    private lateinit var sessionManager: SessionManager
+    private lateinit var repository: ICollegeDetails
+
+    @SuppressLint("LongLogTag")
+    override fun fetchCollegeDetails(code: Int){
+        Log.d(TAG,"<< fetchCollegeDetails()")
+        repository = CollegeDetails()
+        sessionManager = SessionManager(view!!.setContext())
+        view.let {
+            sessionManager.getUserAuthToken()?.let { it1 ->
+                repository.getCollegeDetails(it1, code)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .handelNetworkError()
+                        .subscribe(
+                                { college ->
+                                    view!!.showCollegeDetails(college)
+                                    Log.i(TAG, "Successfully displayed college details. $college")
+                                },
+                                { error ->
+                                    Log.e(TAG, "Failed to get college Details with reason: ${error.message.toString()}")
+                                    Toast.makeText(view!!.setContext(), error.message.toString(), Toast.LENGTH_LONG).show()
+                                })
+            }
+
+        }
+        Log.d(TAG,">> fetchCollegeDetails()")
+    }
+
+    @SuppressLint("LongLogTag")
+    override fun saveCollegeDetails(code: Int, collegeDetails: UpdateCollegeDetails) {
+        Log.d(TAG,"<< saveCollegeDetails()")
+        repository = CollegeDetails()
+        sessionManager = SessionManager(view!!.setContext())
+        Log.d(TAG, " new college details $collegeDetails")
+        sessionManager.getUserAuthToken()?.let {
+            repository.updateCollegeDetails(it, code, collegeDetails)
+                    .handelNetworkError()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            {college ->
+                                view!!.showCollegeDetails(college)
+                                Log.i(TAG, "Successfully updated college details. $college")
+                            },
+                            {error ->
+                                Log.e(TAG, "Failed to update college Details with reason: ${error.message.toString()}")
+                            })}
+        Log.d(TAG,">> saveCollegeDetails()")
+    }
+
+    override fun onDestroy() {
+        view = null
+    }
+}
---
+++ android/MaterialDesignTest/app/src/main/java/com/testexample/materialdesigntest/ui/tpoDashboard/QuestionPaperListFragment.kt
@@ -0,0 +1,97 @@
+package com.testexample.materialdesigntest.ui.tpoDashboard
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.testexample.materialdesigntest.R
+import com.testexample.materialdesigntest.data.network.model.QuestionPaperListResponse
+import com.testexample.materialdesigntest.utils.Constants
+import kotlinx.android.synthetic.main.activity_tpo_dashboard.*
+import kotlinx.android.synthetic.main.fragment_question_paper_list.*
+
+
+class QuestionPaperListFragment : Fragment(R.layout.fragment_question_paper_list), TPODashboardContract.QuestionPaperListView {
+
+    private lateinit var presenter: TPODashboardContract.QuestionPaperListPresenter
+    private var code: Int = 0
+    val TAG = "QuestionPaperListFragment"
+
+    @SuppressLint("LongLogTag")
+    override fun showQuestionPaperList(questionPapers: List<QuestionPaperListResponse>) {
+        Log.d(TAG, "<< showQuestionPaperList()")
+        val questionPaperName = ArrayList<String>()
+        val questionPaperId = ArrayList<String>()
+        val numQuestionPapers = questionPapers.size
+
+        for (i in 0 until numQuestionPapers) {
+            questionPaperName.add(questionPapers[i].paper_name)
+            questionPaperId.add(questionPapers[i].questionPaperId)
+        }
+
+        val listAdapter = QuestionPaperAdapter(this.requireActivity(), questionPaperName, questionPaperId)
+        list.adapter = listAdapter
+
+        Log.d(TAG, ">> showQuestionPaperList()")
+    }
+
+    @SuppressLint("LongLogTag")
+    override fun setPresenter(presenter: TPODashboardContract.QuestionPaperListPresenter) {
+        Log.d(TAG, "<< setPresenter()")
+        this.presenter = presenter
+        Log.d(TAG, ">> setPresenter()")
+    }
+
+    @SuppressLint("LongLogTag")
+    override fun setContext(): Context {
+        Log.d(TAG, "<< setContext()")
+        Log.d(TAG, ">> setContext()")
+        return this.requireContext()
+    }
+
+
+    @SuppressLint("LongLogTag")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d(TAG, "<< onViewCreated()")
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            code = it.getInt(Constants.CODE)
+        }
+
+        presenter = QuestionPaperListPresenter(this)
+        presenter.fetchQuestionPaperList(code)
+
+        list.setOnItemClickListener { _, view, _, _ ->
+            val questionPaperId = (view.findViewById(R.id.description) as TextView).text
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace((requireView().parent as ViewGroup).id, ResultListFragment.newInstance(code, questionPaperId as String),"ResultListFragment")
+                addToBackStack("ResultListFragment")
+                commit()
+            }
+        }
+        Log.d(TAG, ">> onViewCreated()")
+    }
+
+    @SuppressLint("LongLogTag")
+    override fun onDetach() {
+        Log.d(TAG, "onDetach")
+        super.onDetach()
+        requireActivity().tpoDashboardContainer.visibility = View.VISIBLE
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(code: Int) =
+                QuestionPaperListFragment().apply {
+                    arguments = Bundle().apply {
+                        putInt(Constants.CODE, code)
+                    }
+                }
+    }
+
+}

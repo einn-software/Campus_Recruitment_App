7e0068deef05c8d1c036d3b3980d3238231af998
Riya Singhal <60695772+Riya-Singhal15@users.noreply.github.com>
2020-07-28 07:52:48 +0530

    [Task/CRA-182] (#185)
    * Changes in markCalculation
    
    * error handling in marks calculation

--- backend/config/markscalculation.js
+++ backend/config/markscalculation.js
@@ -4,21 +4,23 @@ const Constants = require("../config/constant");
 const QuestionCollections = require("../model/QuestionCollections");
 const AnswerSheet = require("../model/StudentAnswerSheet");
 const QuestionPaper = require("../model/QuestionPaper");
-const {
-  logger
-} = require("./logger");
+const { logger } = require("./logger");
 
 async function fetchNegativeMarks(req, res) {
   let negative_marks = 0;
-  await QuestionPaper.findOne({
-      _id: req.body.question_paper_id
+  await QuestionPaper.findOne(
+    {
+      _id: req.body.question_paper_id,
     },
     (err, result) => {
       if (err || !result) {
-        logger.error(`Function QuestionPaper.findOne({_id: ${req.body.question_paper_id}}, callback) - `, errHandler.idNotFoundErrorHandler('question-paper id'));
+        logger.error(
+          `Function QuestionPaper.findOne({_id: ${req.body.question_paper_id}}, callback) - `,
+          errHandler.idNotFoundErrorHandler("question-paper id")
+        );
         return res
           .status(Constants.er_not_found)
-          .json(errHandler.idNotFoundErrorHandler('question-paper id'));
+          .json(errHandler.idNotFoundErrorHandler("question-paper id"));
       } else {
         negative_marks = result.negative_marking_ratio;
         return negative_marks;
@@ -29,13 +31,16 @@ async function fetchNegativeMarks(req, res) {
 }
 
 async function callStudentResponseList(req, res) {
-  let ans_sheet = await AnswerSheet.find({
-      $and: [{
+  let ans_sheet = await AnswerSheet.find(
+    {
+      $and: [
+        {
           student_id: req.body.student_id,
           question_paper_id: req.body.question_paper_id,
         },
         {
-          $or: [{
+          $or: [
+            {
               state: Constants.markedForReview,
             },
             {
@@ -47,14 +52,12 @@ async function callStudentResponseList(req, res) {
     },
     (err, responseSheet) => {
       if (err || !responseSheet) {
-        logger.error(`Function AnswerSheet.find({$and: [{student_id: ${req.body.student_id},question_paper_id: ${req.body.question_paper_id}},{$or: [{state: ${Constants.markedForReview}},{state: ${Constants.answered}}]}]}, callback) - `, errHandler.idNotFoundErrorHandler('student id or question-paper id'))
-        return res
-          .status(Constants.er_not_found)
-          .json(errHandler.idNotFoundErrorHandler('student id or question-paper id'));
-      } else {
-        const sheet = responseSheet;
-        return sheet;
+        logger.error(
+          `Function AnswerSheet.find({$and: [{student_id: ${req.body.student_id},question_paper_id: ${req.body.question_paper_id}},{$or: [{state: ${Constants.markedForReview}},{state: ${Constants.answered}}]}]}, callback) - `,
+          errHandler.idNotFoundErrorHandler("student id or question-paper id")
+        );
       }
+      return responseSheet;
     }
   );
   return ans_sheet;
@@ -62,15 +65,19 @@ async function callStudentResponseList(req, res) {
 
 async function fetchAnswerByQuestionId(req, res, id) {
   let ans = 0;
-  await QuestionCollections.findOne({
-      _id: id
+  await QuestionCollections.findOne(
+    {
+      _id: id,
     },
     (err, result) => {
       if (err || !result) {
-        logger.error(`Function QuestionCollections.findOne({_id: ${id}}, callback) - `, errHandler.idNotFoundErrorHandler('question id'));
+        logger.error(
+          `Function QuestionCollections.findOne({_id: ${id}}, callback) - `,
+          errHandler.idNotFoundErrorHandler("question id")
+        );
         return res
           .status(Constants.er_not_found)
-          .json(errHandler.idNotFoundErrorHandler('question id'));
+          .json(errHandler.idNotFoundErrorHandler("question id"));
       }
       ans = result.answer;
       return ans;
@@ -80,13 +87,16 @@ async function fetchAnswerByQuestionId(req, res, id) {
 }
 
 async function totalAttemptQuestions(req, res) {
-  let count = await AnswerSheet.find({
-    $and: [{
+  let count = 0;
+  await AnswerSheet.find({
+    $and: [
+      {
         student_id: req.body.student_id,
         question_paper_id: req.body.question_paper_id,
       },
       {
-        $or: [{
+        $or: [
+          {
             state: Constants.markedForReview,
           },
           {
@@ -97,35 +107,36 @@ async function totalAttemptQuestions(req, res) {
     ],
   }).countDocuments((err, length) => {
     if (err || !length) {
-      logger.error(`Function AnswerSheet.find({$and: [{student_id: ${req.body.student_id},question_paper_id: ${req.body.question_paper_id}},{$or: [{state: ${Constants.markedForReview}},{state: ${Constants.answered}}]}]}, callback) - `, errHandler.idNotFoundErrorHandler('student id or question-paper id'))
-      return res
-        .status(Constants.er_not_found)
-        .json(errHandler.idNotFoundErrorHandler('student id or question paper id'));
-    } else {
-      return length;
+      logger.error(
+        `Function AnswerSheet.find({$and: [{student_id: ${req.body.student_id},question_paper_id: ${req.body.question_paper_id}},{$or: [{state: ${Constants.markedForReview}},{state: ${Constants.answered}}]}]}, callback) - `,
+        errHandler.idNotFoundErrorHandler("student id or question-paper id")
+      );
     }
+    count = length;
   });
   return count;
 }
 
 async function calculateMarks(req, res) {
-  let cal = await callStudentResponseList(req, res);
-  let ng = await fetchNegativeMarks(req, res);
   let count = 0;
   let sum = 0;
   let marks = 0;
-  for (let i = 0; i < cal.length; i++) {
-    let selected_anwser = cal[i].selected_option;
-    let id = cal[i].question_id;
-    let ans = await fetchAnswerByQuestionId(req, res, id);
-    if (ans !== selected_anwser) {
-      marks = cal[i].question_max_marks * ng;
-      sum = sum - marks;
-    } else {
-      count = count + 1;
-      marks = cal[i].marks_rewarded = cal[i].question_max_marks;
-      cal[i].save();
-      sum = sum + marks;
+  let cal = await callStudentResponseList(req, res);
+  if (cal != null || []) {
+    let ng = await fetchNegativeMarks(req, res);
+    for (let i = 0; i < cal.length; i++) {
+      let selected_anwser = cal[i].selected_option;
+      let id = cal[i].question_id;
+      let ans = await fetchAnswerByQuestionId(req, res, id);
+      if (ans !== selected_anwser) {
+        marks = cal[i].question_max_marks * ng;
+        sum = sum - marks;
+      } else {
+        count = count + 1;
+        marks = cal[i].marks_rewarded = cal[i].question_max_marks;
+        cal[i].save();
+        sum = sum + marks;
+      }
     }
   }
   logger.info(sum, count);
